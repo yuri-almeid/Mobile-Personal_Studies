edb584768259295b82688d853fd07514d86fe396
Yuri Lima Almeida <yurilima95@gmail.com>
2020-07-24 23:28:50 -0300

    Create the global file with the colors and styles

---
+++ Flutter_app/todoapp/lib/models/global.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+Color darkGreyColor = new Color(0XFF212128);
+//Color redColor = new Color(0XFFE33E57);
+Color redColor = new Color(0XFFDC4F64);
+
+TextStyle intrayTitleStyle = new TextStyle(
+    fontFamily: 'Avenir',
+    //fontWeight: FontWeight.bold,
+    color: darkGreyColor,
+    fontSize: 50);
